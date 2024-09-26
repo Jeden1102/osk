92e3010d957b1db91d6957e212841ce0e3165d2d
jeden1102 <bum123@o2.pl>
2024-09-26 21:03:31 +0200

    master. Animated tabs animation stopping fix + mobile view.

--- src/components/Product/AnimatedTabs.tsx
+++ src/components/Product/AnimatedTabs.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import { ReactNode, useEffect, useState } from "react";
-
+import { Card, CardContent } from "../ui/card";
 type Tab = {
   title: string;
   description: string;
@@ -18,13 +18,17 @@ type AnimatedTabsProps = {
 function AnimatedTabs({ tabs }: AnimatedTabsProps) {
   const [activeTab, setActiveTab] = useState(0);
   const [intervalId, setIntervalId] = useState(0);
+  const [isAnimationStopped, setIsAnimationStopped] = useState(false);
 
   const handleClick = (idx: number) => {
+    setIsAnimationStopped(true);
     setActiveTab(idx);
-    window.clearInterval(intervalId);
   };
 
   useEffect(() => {
+    if (isAnimationStopped) {
+      return () => window.clearInterval(intervalId);
+    }
     const id = window.setInterval(() => {
       const newTab = activeTab < tabs.length - 1 ? activeTab + 1 : 0;
       setActiveTab(newTab);
@@ -32,38 +36,63 @@ function AnimatedTabs({ tabs }: AnimatedTabsProps) {
 
     setIntervalId(id);
     return () => window.clearInterval(id);
-  }, [activeTab]);
+  }, [activeTab, isAnimationStopped]);
 
   return (
-    <div className="flex gap-20 justify-between items-center max-w-[1200px]">
-      <div className="flex flex-col gap-8">
-        {tabs.map((tab, idx) => (
-          <button
-            className={cn(
-              "flex flex-col items-start opacity-50 hover:opacity-75 transition-opacity",
-              activeTab === idx && "opacity-100"
-            )}
-            key={idx}
-            onClick={() => handleClick(idx)}
-          >
-            <span className="flex gap-4 items-center text-lg">
-              {tab.icon} {tab.title}
-            </span>
-            <span className="mt-4">{tab.description}</span>
-          </button>
-        ))}
-      </div>
-      <div className="relative">
-        <span className="absolute -top-1 left-0 h-1 animate-progress-bar bg-gradient-to-r from-primary to-secondary"></span>
-        <Image
-          src={tabs[activeTab].image}
-          alt="Feature image"
-          width={600}
-          height={400}
-          loading="lazy"
-        />
-      </div>
-    </div>
+    <Card className="p-0">
+      <CardContent className="flex gap-20 justify-between items-center max-w-[1200px] p-8">
+        <div className="hidden md:flex flex-col gap-8">
+          {tabs.map((tab, idx) => (
+            <button
+              className={cn(
+                "flex flex-col items-start opacity-50 hover:opacity-75 transition-opacity",
+                activeTab === idx && "opacity-100"
+              )}
+              key={idx}
+              onClick={() => handleClick(idx)}
+            >
+              <span className="flex gap-4 items-center text-lg">
+                {tab.icon} {tab.title}
+              </span>
+              <span className="mt-4">{tab.description}</span>
+            </button>
+          ))}
+        </div>
+        <div className="hidden md:block relative">
+          {!isAnimationStopped && (
+            <span className="absolute hidden md:block -top-1 left-0 h-1 animate-progress-bar bg-gradient-to-r from-primary to-secondary"></span>
+          )}
+          <Image
+            src={tabs[activeTab].image}
+            alt="Feature image"
+            className="rounded-b-md shadow-lg"
+            width={600}
+            height={400}
+            loading="lazy"
+          />
+        </div>
+
+        <div className="md:hidden flex flex-col gap-4">
+          {tabs.map((tab) => (
+            <>
+              <span className="relative rounded-lg flex items-center justify-center w-10 h-10 border u-border-gray-300">
+                {tab.icon}
+              </span>
+              <p>{tab.title}</p>
+              <p className="font-light">{tab.description}</p>
+              <Image
+                src={tab.image}
+                alt="Feature image"
+                className="rounded-b-md shadow-lg"
+                width={600}
+                height={400}
+                loading="lazy"
+              />
+            </>
+          ))}
+        </div>
+      </CardContent>
+    </Card>
   );
 }
 
