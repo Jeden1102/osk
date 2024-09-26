473695a7ebb1e2993414b802cba5217bfeac64c7
jeden1102 <bum123@o2.pl>
2024-09-26 20:32:30 +0200

    master. Animated tabs created.

---
+++ src/components/Product/AnimatedTabs.tsx
@@ -0,0 +1,67 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { ReactNode, useEffect, useState } from "react";
+
+type Tab = {
+  title: string;
+  description: string;
+  icon: ReactNode;
+  image: string;
+};
+
+type AnimatedTabsProps = {
+  tabs: Tab[];
+};
+
+function AnimatedTabs({ tabs }: AnimatedTabsProps) {
+  const [activeTab, setActiveTab] = useState(0);
+  const [intervalId, setIntervalId] = useState(0);
+
+  const handleClick = (idx: number) => {
+    setActiveTab(idx);
+    window.clearInterval(intervalId);
+  };
+
+  useEffect(() => {
+    const id = window.setInterval(() => {
+      const newTab = activeTab < tabs.length - 1 ? activeTab + 1 : 0;
+      setActiveTab(newTab);
+    }, 5000);
+
+    setIntervalId(id);
+    return () => window.clearInterval(id);
+  }, [activeTab]);
+
+  return (
+    <div className="flex gap-20 justify-between items-center max-w-[1200px]">
+      <div className="flex flex-col gap-8">
+        {tabs.map((tab, idx) => (
+          <button
+            className={cn(
+              "flex flex-col items-start opacity-50 hover:opacity-75 transition-opacity",
+              activeTab === idx && "opacity-100"
+            )}
+            key={idx}
+            onClick={() => handleClick(idx)}
+          >
+            <span className="flex gap-4 items-center text-lg">
+              {tab.icon} {tab.title}
+            </span>
+            <span className="mt-4">{tab.description}</span>
+          </button>
+        ))}
+      </div>
+      <Image
+        src={tabs[activeTab].image}
+        alt="Feature image"
+        width={600}
+        height={400}
+        loading="lazy"
+      />
+    </div>
+  );
+}
+
+export default AnimatedTabs;
--- src/components/Product/FeaturesSlides.tsx
+++ src/components/Product/FeaturesSlides.tsx
@@ -1,11 +1,37 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { useTranslations } from "next-intl";
-
+import AnimatedTabs from "./AnimatedTabs";
+import { HiOutlineUsers } from "react-icons/hi2";
+import { PiCarSimpleLight } from "react-icons/pi";
+import { IoStatsChartOutline } from "react-icons/io5";
 function FeaturesSlides() {
   const t = useTranslations();
 
+  const tabs = {
+    admin: [
+      {
+        title: "Test title 1",
+        description: "test descirption",
+        icon: <HiOutlineUsers />,
+        image: "/images/blog/blog-01.jpg",
+      },
+      {
+        title: "Test title 2",
+        description: "test descirption",
+        icon: <PiCarSimpleLight />,
+        image: "/images/blog/blog-02.jpg",
+      },
+      {
+        title: "Test title 3",
+        description: "test descirption",
+        icon: <IoStatsChartOutline />,
+        image: "/images/blog/blog-03.jpg",
+      },
+    ],
+  };
+
   return (
-    <Tabs defaultValue="admin" className="w-full mt-4">
+    <Tabs defaultValue="admin" className="w-full my-8">
       <TabsList className="mx-auto">
         <TabsTrigger value="admin">{t("Features.for_admin")}</TabsTrigger>
         <TabsTrigger value="instructor">
@@ -14,20 +40,15 @@ function FeaturesSlides() {
         <TabsTrigger value="student">{t("Features.for_student")}</TabsTrigger>
       </TabsList>
       <TabsContent value="admin">
-        <div className="-mx-4 mt-12 grid md:grid-cols-2 lg:grid-cols-3  lg:mt-20 gap-4">
-          admin
-          {/* @todo slides lookalike- > https://volta.net/ */}
+        <div className="-mx-4 mt-12 lg:mt-20">
+          <AnimatedTabs tabs={tabs.admin} />
         </div>
       </TabsContent>
       <TabsContent value="instructor">
-        <div className="-mx-4 mt-12 grid md:grid-cols-2 lg:grid-cols-3  lg:mt-20 gap-4">
-          instructor
-        </div>
+        <div className="-mx-4 mt-12 lg:mt-20">instructor</div>
       </TabsContent>
       <TabsContent value="student">
-        <div className="-mx-4 mt-12 grid md:grid-cols-2 lg:grid-cols-3  lg:mt-20 gap-4">
-          student
-        </div>
+        <div className="-mx-4 mt-12 lg:mt-20">student</div>
       </TabsContent>
     </Tabs>
   );
